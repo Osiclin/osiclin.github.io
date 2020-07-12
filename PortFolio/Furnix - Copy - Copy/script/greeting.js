875eb8585d5edd03694504aefa338f691d627e52
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-07-12 23:27:25 +0100

    add furnix

---
+++ PortFolio/Furnix - Copy - Copy/script/greeting.js
@@ -0,0 +1,18 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting; 
+    if (hourNow >= 0 && hourNow < 12){
+        greeting= "GOOD MORNING!";
+    }
+    else if(hourNow >= 12 && hourNow < 18){
+     greeting = "GOOD AFTERNOON!";
+    }
+    else if(hourNow >= 18 && hourNow < 24){
+     greeting = "GOOD EVENING!";
+    }
+    else {
+        greeting = "WELCOME!";
+    }
+
+var el = document.getElementById('greeting');
+el.textContent = greeting;
