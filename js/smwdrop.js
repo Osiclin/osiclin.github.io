8a9a6bdfacd2124e786f56cab48a7dc7ea50d23b
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-08-04 17:08:40 +0100

    remove pics

---
+++ js/smwdrop.js
@@ -0,0 +1,11 @@
+/*drop see my works*/
+function smwdrop (){
+var el = document.getElementById('rs').style.display = 'block';
+}
+
+var el1 = document.getElementById('mb');
+el1.addEventListener('click', smwdrop, false);
+
+
+/*onclick="document.getElementById('rs').style.display= 'block'" 
+onclick="document.getElementById('rs').style.display= 'none'"*/
