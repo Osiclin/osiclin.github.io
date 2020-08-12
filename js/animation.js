31165c527f9edb2bbc4295c9a349621a00ed3cfb
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-08-12 09:46:22 +0100

    update logo font & added jquery effects

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+$('.aaa').hide().delay(600).fadeIn(1000);
+$('.aaaa').hide().delay(400).fadeIn(1000);
+$('.aaaaa').hide().fadeIn(1000);
+$('.mypics').hide().delay(1000).fadeIn(1500);
+$('.msg1').hide().delay(300).slideDown(500);
+$('.msg2').hide().delay(500).slideDown(600);
+$('.msg3').hide().delay(700).slideDown(700);
+
+$('#hb').on('click', function() {
+    $('#myLinks').slideToggle(600);
+})
