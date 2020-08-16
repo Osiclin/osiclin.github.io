d3f80101c483bb2da196ce1f181bda488d4f7be0
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-08-16 01:31:11 +0100

    test copy file

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 $('.aaa').hide().delay(600).fadeIn(1000);
 $('.aaaa').hide().delay(400).fadeIn(1000);
-$('.aaaaa').hide().fadeIn(1000);
+$('.aaaaa').hide().fadeIn(500);
 $('.mypics').hide().delay(1000).fadeIn(1500);
 $('.msg1').hide().delay(300).slideDown(500);
 $('.msg2').hide().delay(500).slideDown(600);
