1f42959fb02c88b6c9ee2b2ca566242a54b58d06
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-08-23 12:38:07 +0100

    animation

--- js/animation.js
+++ js/animation.js
@@ -9,3 +9,18 @@ $('.msg3').hide().delay(700).slideDown(700);
 $('#hb').on('click', function() {
     $('#myLinks').slideToggle(600);
 })
+
+/*$(window).on('scroll', function() {
+    var $window = $(window);
+    var $slideText = $('#uwSlide');
+    if ($window.outerWidth < 481 && $window.height >= 744) {
+        $slideText.animate({'right': '0px'}, 250);
+    } else {
+        $slideText.stop(true).animate({'right': '360px'}, 250);
+    }
+});*/
+
+$('.furnix').on('click', function() {
+    $('.furnix').animate({right: '250px'});
+});
+
