aab1f9b7867da46cef1fb052a9d575cefc3219f8
osita <67206380+Osiclin@users.noreply.github.com>
2020-09-12 06:55:53 +0100

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -19,52 +19,57 @@ $('#hb').on('click', function() {
 })
 
 
-/*$(window).on('scroll', function() {
-    var $window = $(window);
-    var $slideText = $('#uwSlide');
-    if ($window.outerWidth < 481 && $window.height >= 744) {
-        $slideText.animate({'right': '0px'}, 250);
-    } else {
-        $slideText.stop(true).animate({'right': '360px'}, 250);
-    }
-});*/
-
-var furnix = document.getElementById('furnix');
-var furnix1 = document.getElementById('furnix1');
-var furnix2 = document.getElementById('furnix2');
+var furnix = document.getElementsByClassName('furnix');
 var hr = document.getElementById('hr');
 var rworks = document.getElementById('rworks');
 
-furnix.style.display = 'none';
-furnix1.style.display = 'none';
-furnix2.style.display = 'none';
+for (i = 0; i < furnix.length; i++){
+    furnix[i].style.display = 'none';
+}
+
 hr.style.display = 'none';
 rworks.style.display = 'none';
 
 
 window.addEventListener('scroll', () => {
-    if ( document.body.scrollTop > 120 || document.documentElement.scrollTop > 120 ) {
-        furnix.style.display = 'block';
-        furnix.classList.add('slideleft');
-        $('.projectinfo').fadeIn(2000);
-    }
-    if ( document.body.scrollTop > 450 || document.documentElement.scrollTop > 450 ) {
-        furnix1.style.display = 'block';
-        furnix1.classList.add('slideleft');
-    }
-    if ( document.body.scrollTop > 750 || document.documentElement.scrollTop > 750 ) {
-        furnix2.style.display = 'block';
-        furnix2.classList.add('slideleft');
-    }
-    if ( document.body.scrollTop > 90 || document.documentElement.scrollTop > 90 ) {
+    if ( window.outerWidth <= 480 && document.body.scrollTop > 120 || document.documentElement.scrollTop > 120 ) {
+            furnix[0].classList.add('slideleft');
+            furnix[0].style.display = 'block';
+            $('.projectinfo').fadeIn(1000);
+        }
+    if ( window.outerWidth <= 480 && document.body.scrollTop > 450 || document.documentElement.scrollTop > 450 ) {
+            furnix[1].classList.add('slideleft');
+            furnix[1].style.display = 'block';
+        }
+    if ( window.outerWidth <= 480 && document.body.scrollTop > 750 || document.documentElement.scrollTop > 750 ) {
+            furnix[2].classList.add('slideleft');
+            furnix[2].style.display = 'block';
+        }
+    if ( window.outerWidth <= 480 && document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
         hr.style.display = 'block';
         hr.classList.add('slideright');
     }
-    if ( document.body.scrollTop > 90 || document.documentElement.scrollTop > 90 ) {
+    if ( window.outerWidth <= 480 && document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
         $('#rworks').fadeIn(1000);
     }
 })
 
+window.addEventListener('scroll', () => {
+    if ( window.outerWidth > 480 && document.body.scrollTop > 230 || document.documentElement.scrollTop > 230 ) {
+        for (i = 0; i < furnix.length; i++){
+            furnix[i].classList.add('slideup');
+        }
+    }
 
-
-
+    if ( window.outerWidth > 480 && document.body.scrollTop > 270 || document.documentElement.scrollTop > 270 ) {
+            $('.projectinfo').delay(50000).fadeIn(2000);
+    }
+    
+    if ( window.outerWidth > 480 && document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+        hr.classList.add('slideright');
+        hr.style.display = 'block';
+    }
+    if ( window.outerWidth > 480 && document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+        $('#rworks').fadeIn(1000);
+    }
+})
