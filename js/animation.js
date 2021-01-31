477494ae7b9321ac2e979ae3c6441dbd134797bb
osita <67206380+Osiclin@users.noreply.github.com>
2021-02-01 00:57:41 +0100

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -40,6 +40,7 @@ function animateArrow() {
 animateArrow();
 
 
+
 var furnix = document.getElementsByClassName('furnix');
 var furnix1 = document.getElementsByClassName('project-block');
 var rwhr = document.getElementById('rwhr');
@@ -130,28 +131,132 @@ window.addEventListener('scroll', () => {
 
 
 //change theme
-// const toggle = document.getElementById('toggle');
-// const msg = document.getElementById('msg');
-// const toggleSwitch = document.getElementById('toggle-switch');
-// const body = document.getElementsByTagName('body');
+const toggle = document.getElementById('toggle');
+const msg = document.getElementById('msg');
+const toggleSwitch = document.getElementById('toggle-switch');
+const body = document.getElementsByTagName('body');
+const sbt = document.getElementsByClassName('service-block');
+const iconS = document.getElementsByClassName('icon-s');
+const shead = document.getElementsByClassName('service-head');
+const sname = document.getElementById('sname');
+const rw = document.getElementsByClassName('rw');
+const serv = document.getElementsByClassName('serv');
+const mypics = document.getElementsByClassName('mypics');
+const surface = document.getElementById('surface');
+const fas = document.getElementsByClassName('fas');
+const fab = document.getElementsByClassName('fab');
+const hr = document.getElementsByTagName('hr');
+const fa = document.getElementsByClassName('fa-angle-down');
+const foot = document.getElementById('foot');
+const myname = document.getElementsByClassName('myname');
+const header = document.getElementsByTagName('header');
+const topnav = document.getElementsByClassName('topnav');
+const activ = document.getElementsByClassName('activ');
 
-// toggle.addEventListener('click',()=>{
-//     if(toggle.className == 'toggle-white'){
-//         toggle.classList.remove('toggle-white');
-//         toggleSwitch.classList.remove('toggle-switch-black');
-//         toggle.classList.add('toggle-black');
-//         toggleSwitch.classList.add('toggle-switch-white');
-//         body[0].style.backgroundColor = "skyblue";
-//         body[0].style.color = "black";
-//         msg.style.color = "black";
-//     }
-//     else{
-//         toggle.classList.remove('toggle-black');
-//         toggleSwitch.classList.remove('toggle-switch-white');
-//         toggle.classList.add('toggle-white');
-//         toggleSwitch.classList.add('toggle-switch-black');
-//         body[0].style.backgroundColor = "black";
-//         body[0].style.color = "white";
-//         msg.style.color = "white";
-//     }
-// }, false);
+toggle.addEventListener('click',()=>{
+    if(toggle.className == 'toggle-white'){
+        toggle.classList.remove('toggle-white');
+        toggleSwitch.classList.remove('toggle-switch-black');
+        toggle.classList.add('toggle-black');
+        toggleSwitch.classList.add('toggle-switch-white');
+        body[0].style.backgroundColor = "#e6e2c8";
+        body[0].style.color = "#474646";
+        msg.style.color = "black";
+        for(i=0; i<sbt.length; i++) {
+            sbt[i].classList.add('service-block-theme');
+            iconS[i].classList.add('icon-s-theme');
+            shead[i].classList.add('service-head-theme');
+        }
+        sname.style.color = "black";
+        serv[0].style.backgroundColor = "#e6e2c8";
+        surface.style.backgroundColor = "gray";
+        rw[0].style.backgroundColor = "#e6e2c8";
+        mypics[0].style.filter = "grayscale()";
+        for(a = 0; a < furnix.length; a++){
+            furnix[a].style.filter = "grayscale()";
+        }
+        for(b = 0; b < 4; b++){
+            fas[b].classList.add('fas1');
+            fab[b].classList.add('fab1');
+        }
+        for(c = 0; c < 5; c++){
+            hr[c].style.backgroundColor = "turquoise";
+            hr[c].style.color = "turquoise";
+            hr[c].style.borderColor = "turquoise";
+        }
+        for(d = 0; d < fa.length; d++){
+            fa[d].style.color = "black";
+        }
+        foot.style.color = "black";
+        myname[0].classList.add('myname1');
+        if ( window.outerWidth <= 480 ) {
+            header[0].style.backgroundColor = "#e6e2c8";
+            topnav[0].style.backgroundColor = "black";
+            activ[0].style.backgroundColor = "#e6e2c8";
+        }
+        hb.style.color = "black";
+        hb.style.backgroundColor = "#e6e2c8";
+    }
+    else{
+        toggle.classList.remove('toggle-black');
+        toggleSwitch.classList.remove('toggle-switch-white');
+        toggle.classList.add('toggle-white');
+        toggleSwitch.classList.add('toggle-switch-black');
+        body[0].style.backgroundColor = "black";
+        body[0].style.color = "white";
+        msg.style.color = "white";
+        for(i=0; i<sbt.length; i++) {
+            sbt[i].classList.remove('service-block-theme');
+            iconS[i].classList.remove('icon-s-theme');
+            shead[i].classList.remove('service-head-theme');
+            sname.style.color = "turquoise";
+        }
+        serv[0].style.backgroundColor = "black";
+        surface.style.backgroundColor = "gray";
+        rw[0].style.backgroundColor = "black";
+        mypics[0].style.filter = "none";
+        for(a = 0; a < furnix.length; a++){
+            furnix[a].style.filter = "none";
+        }
+        for(b = 0; b < 4; b++){
+            fas[b].classList.remove('fas1');
+            fab[b].classList.remove('fab1');
+        }
+        for(c = 0; c > 2; c++){
+            hr[c].style.backgroundColor = "turquoise";
+            hr[c].style.color = "turquoise";
+            hr[c].style.borderColor = "turquoise";
+        }
+        for(d = 0; d < fa.length; d++){
+            fa[d].style.color = "turquoise";
+        }
+        foot.style.color = "white";
+        myname[0].classList.remove('myname1');
+        if ( window.outerWidth <= 480 ) {
+            header[0].style.backgroundColor = "black";
+            topnav[0].style.backgroundColor = "black";
+            activ[0].style.backgroundColor = "black";
+        }
+        hb.style.color = "turquoise";
+        hb.style.backgroundColor = "black";
+    }
+    
+}, false);
+
+
+
+var text = "<html>";
+var textType = document.getElementById('texttype');
+
+text += '&lt!DOCTYPE html&gt <br>';
+text += '&lthtml&gt <br>';
+text += "&lttitle&gtcoding & programming&lttitle&gt <br>";
+text += '&ltstyle type="text/css"&gt <br>';
+text += "body{ <br>";
+text += "font-family: cursive; <br>";
+text += "font-size: 2rem; <br>";
+text += "&lt/style&gt <br>";
+text += "&lt/head&gt <br>";
+text += "&lt/html&gt ";
+
+textType.innerHTML = text;
