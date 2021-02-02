ec9c8b61f1aa89f0b9d5638c18cfff3c68d9e1e5
osita <67206380+Osiclin@users.noreply.github.com>
2021-02-02 22:16:33 +0100

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -74,30 +74,30 @@ srhr.style.visibility = 'hidden';
 // mobile effect
 if ( window.outerWidth <= 480 ) {
 window.addEventListener('scroll', () => {
-    if ( window.pageYOffset + 550 >= rwhrposition ){
+    if ( window.pageYOffset + 200 >= rwhrposition ){
         rwhr.classList.add('slideright');
         rwhr.style.visibility = 'visible';  
     }
-    if ( window.pageYOffset + 550 >= srhrposition ){
+    if ( window.pageYOffset + 200 >= srhrposition ){
         srhr.classList.add('slideright');
         srhr.style.visibility = 'visible';
     }
-    if ( window.pageYOffset + 580 >= furnix1position ) {
+    if ( window.pageYOffset + 230 >= furnix1position ) {
         furnix1[0].classList.add('slideup');
         furnix[0].style.visibility = 'visible';
         projectinfo[0].style.visibility = 'visible';
     }
-    if ( window.pageYOffset + 580 >= furnix2position ) {
+    if ( window.pageYOffset + 230 >= furnix2position ) {
         furnix1[1].classList.add('slideup');
         furnix[1].style.visibility = 'visible';
         projectinfo[1].style.visibility = 'visible';
     }
-    if ( window.pageYOffset + 580 >= furnix3position ) {
+    if ( window.pageYOffset + 230 >= furnix3position ) {
         furnix1[2].classList.add('slideup');
         furnix[2].style.visibility = 'visible';
         projectinfo[2].style.visibility = 'visible';
     }
-    if ( window.pageYOffset + 580 >= furnix4position ) {
+    if ( window.pageYOffset + 230 >= furnix4position ) {
         furnix1[3].classList.add('slideup');
         furnix[3].style.visibility = 'visible';
         projectinfo[3].style.visibility = 'visible';
@@ -192,7 +192,7 @@ toggle.addEventListener('click',()=>{
         if ( window.outerWidth <= 480 ) {
             header[0].style.backgroundColor = "#e6e2c8";
             topnav[0].style.backgroundColor = "black";
-            // activ[0].style.backgroundColor = "#e6e2c8";
+            activ[0].style.backgroundColor = "#e6e2c8";
         }
         hb.style.color = "black";
         hb.style.backgroundColor = "#e6e2c8";
